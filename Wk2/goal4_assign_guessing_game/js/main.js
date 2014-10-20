2d3d06748da008be8224b32d7e9438178fbd8ae2
Albert Martinez <albertm@fullsail.edu>
2014-10-20 09:37:33 -0700

    Upload guessing game code assignment

---
+++ Wk2/goal4_assign_guessing_game/js/main.js
@@ -0,0 +1,36 @@
+/**
+ * GUESSING GAME:
+ *
+ * Created By: Albert Martinez
+ * Date: 10/20/14
+ * 
+ * GUESSING GAME
+ */
+
+//Game variables
+(function (){
+
+var ranNum = Math.floor(Math.random()*11);
+var dom = {
+	output: document.getElementById('output'),
+	input: document.getElementById('input'),
+	button: document.getElementsByTagName('button')
+};
+
+button.addEventListener("click", ranNum, function(){
+	if(isNaN()validate
+});
+
+// Create an .addEventListner event listener on the guess button to listen for a click event.
+// When the guess button is clicked, evaluate your guess with the number the computer has chosen.
+// Create a function to validate the users input:
+// Validate that a number was entered -- AND
+// Validate that the number entered is between 1 and 10
+// If your guess is higher than the computer's number then display appropriate message in the HTML.
+// If your guess is lower than the computer's number then display appropriate message in the HTML.
+// If your guess is equal to the computer's number then display appropriate message and end the game.
+// Tally all the wrong guesses and end the game after the 3rd incorrect guess and show appropriate game over message in the HTML.
+// Deactivate the button by removing the event listener when the game is over.
+// EXTRA CREDIT: Use a keyboard event for the ENTER button that also acts like clicking on the guess button.
+
+})();

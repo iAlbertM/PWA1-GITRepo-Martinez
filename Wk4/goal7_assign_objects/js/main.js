36159697354356b02b416b2d77a624122826ce72
Albert Martinez <albertm@fullsail.edu>
2014-10-24 21:25:14 -0700

    upload final assignment

--- Wk4/goal7_assign_objects/js/main.js
+++ Wk4/goal7_assign_objects/js/main.js
@@ -1,6 +1,47 @@
 /**
- * Created by the JavaScript Development Team
- * Class: PWA
- * Goal: Goal7
+ * Created by Albert Martinez
+ * Class: PWA-1
+ * Goal: Goal7 Assignment
  */
 
+// self-executing anonymous function 
+(function(){
+	// define all variables
+	var numPeople = 3;
+	var names = ["Matt Hatter", "Chris Mass", "Will Hisoneway", "Scott Winters", "Sandy Sikek"];
+	var people = [];
+	var interval;
+
+	for (var i = 0; i < numPeople; i++) {
+		// generate random number between 0 and the length of the names array
+		var randNum = Math.floor(Math.random() * names.length);
+		// creating a variable that stores generating new Person objects
+		var person = new Person(names[randNum], i + 1);
+
+		// inserting data into the correct columns Names in col 1, jobs in col 2
+		populateHTML(person.name, "r" + (i + 1) + "c1")
+		populateHTML(person.job, "r" + (i + 1) + "c2")
+		
+		// place the completed person into the people array per instructions
+		people.push(person);
+		names.splice(randNum, 1);
+	} 
+
+	// make sure to clear the interval since we only want 1  at any given time
+	clearInterval(interval);
+	// assigning the variable interval to the function that sets up the interval
+	interval = setInterval(runUpdate, 1000 / 30);
+	
+	function populateHTML(pName, pfield) {
+		var rowCol = document.getElementById(pfield);
+		rowCol.innerHTML = pName;
+	}
+	function runUpdate(){
+		//run update for each element in the people array - hence each Person instance
+		people.forEach(function(element){
+			// calling the Person.prototype.update method from person.js file 
+			element.update();
+		});
+	}
+	console.log(people);
+})();
--- Wk4/goal7_assign_objects/js/person.js
+++ Wk4/goal7_assign_objects/js/person.js
@@ -1,5 +1,47 @@
-/**
- * Created by the JavaScript Development Team
- * Class: PWA
- * Goal: Goal7
+/*
+ * Created by Albert Martinez
+ * Class: PWA-1
+ * Goal: Goal 7 week 4
  */
+
+ (function(){
+
+	// add the Person object to the global window object so that we can ready Person from main.js
+	window.Person = Person;
+
+	// list all job opportunities and actions that a Person do
+	Person.jobs = ["Graphic Artist", "Illustrator", "Web Developer", "Web Designer", "Hair Stylist" ];
+	// list all actions that the user can do 
+	Person.actions = ["Sketching", "Taking a break", "Meditating", "Coding", "Studying"];
+
+	//screen cast link to watch when in need of var "https://www.youtube.com/watch?v=5wWdKr86MIc&list=PLhRlE5h84yWb2AmOu30rGN6XTDmRyvUbn#t=3178";
+	// create the Person object using contructor function
+	function Person(name, row){
+		// check function is being seen by using console.log to print the name of the person it creates
+		console.log("Person Created: ", name);
+		this.name 	= name;
+		// setting the initial random action from the Person.actions array
+		this.action = Person.actions[Math.floor(Math.random()*Person.actions.length)];
+		// setting the initial random job choice from the Person.jobs array
+		this.job 	= Person.jobs[Math.floor(Math.random()*Person.jobs.length)];
+		this.row 	= row;
+		// setting variable col3 to the HTML element in column 3 row: (depends on the element calling the function = this.row)
+		var col3 = document.getElementById("r" + this.row + "c3");
+		// column 3 is the action column so naturally we are placing the objects .action here
+		col3.innerHTML = this.action;
+	};
+	
+	Person.prototype.update = function(){
+		// run the function about once every 5 seconds or so( 1 / 60sec = 0.01)
+		if(Math.floor(Math.random() < 0.01)) {
+			// random number between 0 and the length of the Person.actions array: 5
+			var i = Math.floor(Math.random()*Person.actions.length);
+			// setting the actions index to be random using the i variable
+			this.action = Person.actions[i];
+			// grab the html element to insert the data into column 3 of this Person.row
+			var col3 = document.getElementById("r" + this.row + "c3");
+			col3.innerHTML = this.action;
+		}
+	}
+})();
+
